2356a70362d521a37a8a692b0d3a6fea1a479449
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-04-28 23:57:50 +0530

    CRUD Step 2

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,10 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.listen(PORT, () => {
   console.log(`Server at http://localhost:${PORT}`);
 });
